e4afc006476aea3e13efab064efa7552d6bf4de3
rexdominicperico20 <rexdominicperico20@gmail.com>
2026-07-27 13:23:53 +0800

    Use AUP RFID profile for attendance matching

--- allow_unregistered_attendance.sql
+++ allow_unregistered_attendance.sql
@@ -1,2 +1,6 @@
 alter table if exists public.attendance
     alter column participant_id drop not null;
+
+alter table if exists public.attendance
+    add column if not exists attendance_course text,
+    add column if not exists attendance_source text;
